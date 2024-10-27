69c0882e56da0a9e95daa52f7e235ed23f6fcf45
AlexShutro <117032622+AlexShutro@users.noreply.github.com>
2024-10-27 17:28:33 +0300

    Add files via upload

---
+++ KS/labs/LAB6/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Net;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("IP: ");
+            if (IPAddress.TryParse(Console.ReadLine(), out IPAddress? ip))
+            {
+                Console.Write("Mask: ");
+                if (IPAddress.TryParse(Console.ReadLine(), out IPAddress? mask))
+                {
+                    byte[] bMask = mask.GetAddressBytes();
+                    byte checker = 0b_10000000;
+                    bool changed = false;
+                    bool different = false;
+                    bool condition = true;
+
+                    for (int i = 0; i < bMask.Length; i++)
+                    {
+                        if (!condition)
+                        {
+                            break;
+                        }
+
+                        checker = 0b_10000000;
+                        for (int j = 0; j < 8; j++)
+                        {
+                            if (!different && changed)
+                            {
+                                condition = false;
+                                break;
+                            }
+
+                            if ((bMask[i] & checker) == 0)
+                            {
+                                changed = true;
+                                different = true;
+                            }
+                            else
+                            {
+                                different = false;
+                            }
+
+                            checker = (byte)(checker >> 1);
+                        }
+                    }
+
+                    if (condition)
+                    {
+                        Console.WriteLine("\nIP address and mask are valid.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nMask address is invalid.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("\nInvalid mask address!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid IP address!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
